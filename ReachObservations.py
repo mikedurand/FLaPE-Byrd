72d55b6436b028bc34b8b3233e2283332e9328f2
mikedurand <durand.8@osu.edu>
2022-02-02 23:08:38 -0500

    Update ReachObservations.py
    Added three sub-domain fit to height-width data

--- ReachObservations.py
+++ ReachObservations.py
@@ -5,18 +5,18 @@ Created on Wed Jan 13 23:49:40 2021
 @author: mtd
 """
 
-from numpy import reshape,concatenate,zeros,ones,triu,empty,arctan,tan,pi,std,mean,sqrt,var,cov
-from scipy import stats
+from numpy import reshape,concatenate,zeros,ones,triu,empty,arctan,tan,pi,std,mean,sqrt,var,cov,inf,polyfit,linspace,array
+from scipy import stats,optimize
 import matplotlib.pyplot as plt
 import copy
 
 class ReachObservations:    
         
-    def __init__(self,D,RiverData,ConstrainHWSwitch=False):
+    def __init__(self,D,RiverData,ConstrainHWSwitch=False,Calc3SD_HWFits=False,Verbose=False):
         
         self.D=D    
         self.ConstrainHWSwitch=ConstrainHWSwitch
-        
+        self.Verbose=Verbose
         
         # assign data from input dictionary
         self.h=RiverData["h"]           
@@ -26,10 +26,16 @@ class ReachObservations:
         self.sigh=RiverData["sigh"]
         self.sigw=RiverData["sigw"]
         self.sigS=RiverData["sigS"]    
+
+        # calculate H-W fits for 3 sub-domain using EIV model a la SWOT
+        if Calc3SD_HWFits:
+            self.CalcHWFits()
         
         # constrain heights and widths to be self-consistent
-        #      note - h,w data only overwritten if switch set to true
-        self.ConstrainHW()
+        if Calc3SD_HWFits:
+             print('hello')
+        else:
+             self.ConstrainHW()
 
         #%% create resahepd versions of observations
         self.hv=reshape(self.h, (self.D.nR*self.D.nt,1) )
@@ -69,7 +75,7 @@ class ReachObservations:
         [m,b]=self.FitEIV(xn,yn)
         mo=-tan(pi/2-arctan(m))
  
-        #projet w,h onto LOC
+        #projet w,h onto LOC or EIV
         hhatn=(yn-mo*xn-b)/(m-mo)
         whatn=m*hhatn+b
 
@@ -162,3 +168,193 @@ class ReachObservations:
         beta0hat=mean(y)-beta1hat*mean(x)
 
         return beta1hat, beta0hat
+
+    def CalcHWFits(self,r=0):
+
+        import warnings
+        warnings.filterwarnings("ignore", message="delta_grad == 0.0. Check if the approximated function is linear.")
+
+        # this computes the SWOT-like height-width fit
+
+ 	# r : reach ID
+
+        # outer level parameter vector:
+        # po = Hb0,Hb1 i.e. WSE breakpoint 0, then WSE breakpoint 1
+        # inner level parameter vector:
+        # pi = p00, p01, p10, p11, p20,p21 i.e. p[domain 0][coefficient 0], p[domain 0][coefficient 1], p[domain 1][coefficient 0],...
+
+        #0 check uncertainties
+        if self.sigw<0:
+             self.sigw=10 
+
+        #1 choose initial parameters for outer loop
+        WSEmin=min(self.h[r,:])
+        WSEmax=max(self.h[r,:])
+        WSErange=WSEmax-WSEmin
+        WSErange=WSEmax-WSEmin
+        init_params_outer=[WSEmin+WSErange/3, WSEmin+2*WSErange/3]
+
+        #2 compute a solution where we set the breakpoints at 1/3 of the way through the domain
+        ReturnSolution=True
+        Jset,p_inner_set=SSE_outer(init_params_outer,self.h[r,:],self.w[r,:],ReturnSolution,self.sigh,self.sigw,self.Verbose)
+
+        if self.Verbose:
+            print('height-width fit for set breakpoints')
+            plot3SDfit(self.h[r,:],self.w[r,:],p_inner_set,init_params_outer)
+
+        #3 optimize both inner and outer loop simultaneously
+
+        #3.1 parameter bounds
+        nparams_outer=len(init_params_outer)
+        lb=zeros(nparams_outer,)
+        ub=zeros(nparams_outer,)
+
+        lb[0]=WSEmin+WSErange*0.1
+        ub[0]=WSEmin+WSErange*0.9
+        lb[1]=WSEmin+WSErange*0.1
+        ub[1]=WSEmin+WSErange*0.9
+
+        param_bounds_outer=optimize.Bounds(lb,ub)
+
+        #3.2 constrain breakpoints to be monotonic
+        A=array([[1,-1]])
+        constraint2=optimize.LinearConstraint(A,-inf,-0.1)    
+
+        ReturnSolution=False
+        res = optimize.minimize(fun=SSE_outer,
+                    x0=init_params_outer,
+                    args=(self.h[r,:],self.w[r,:],ReturnSolution,self.sigh,self.sigw,self.Verbose),
+                    bounds=param_bounds_outer,
+                    method='trust-constr',    
+                    constraints=constraint2,
+                    options={'disp':self.Verbose,'maxiter':1e2,'verbose':0})
+
+        params_outer_hat=res.x
+
+        ReturnSolution=True
+        [Jnest,params_inner_nest]=SSE_outer(params_outer_hat,self.h[r,:],self.w[r,:],ReturnSolution,self.sigh,self.sigw,self.Verbose)
+
+        if self.Verbose:
+             print('height-width fit for nested optimization')
+             plot3SDfit(self.h[r,:],self.w[r,:],params_inner_nest,params_outer_hat)
+
+        if res.success or (Jnest<Jset):
+             print('nested optimiztion sucess:',res.success)
+             print('nested objective:',Jnest)
+             print('set objective function:',Jset)
+             print('using nested solution')
+             self.Hbp=params_outer_hat
+             self.HWparams=params_inner_nest
+        else:
+             self.Hbp=init_params_outer
+             self.HWparams= p_inner_set
+
+        return 
+
+def ChooseInitParamsInner(h,w):
+    #function to choose initial parameters describing SWOT-like height-width fit
+
+    p1SD=polyfit(h,w,1) #slope and intercept for one sub-domain    
+
+    init_params_inner=[p1SD[0],p1SD[1],p1SD[0],p1SD[1],p1SD[0],p1SD[1]]
+
+    nparams_inner=len(init_params_inner)
+    
+    return init_params_inner,nparams_inner
+
+def SetInnerParamBounds(nparams):
+    #function to set initial parameter bounds describing SWOT-like height-width fit
+    
+    lb=zeros(nparams,)
+    ub=zeros(nparams,)
+      
+    # slope values are 0,2,4. intercept values are 1,3,5
+        
+    # slope lower bound is zero, but upper bound is inf
+    ub[0]=inf
+    ub[2]=inf
+    ub[4]=inf
+    
+    lb[0]=0 
+    lb[2]=0
+    lb[4]=0
+    
+    # intercept does not have bounds    
+    lb[1]=-inf
+    lb[3]=-inf
+    lb[5]=-inf
+    
+    ub[1]=inf
+    ub[3]=inf    
+    ub[5]=inf
+    
+    return lb,ub
+
+# define outer objective function, with inner objective function nested within
+def SSE_outer(param_outer,h,w,ReturnSolution,sigh,sigw,Verbose):
+    
+    [init_params_inner,nparams_inner]=ChooseInitParamsInner(h,w)
+    
+    [lb,ub]=SetInnerParamBounds(nparams_inner)
+
+    param_bounds_inner=optimize.Bounds(lb,ub)
+
+    def SSE_inner(inner_params,xbreak,h,w,sigh,sigw):            
+        
+        i0=h<xbreak[0]                
+        J0=(sigw**2 + inner_params[0]**2 * sigh**2)**-1*sum((h[i0]*inner_params[0]+inner_params[1]-w[i0])**2 )
+        i1=(h>=xbreak[0]) & (h<xbreak[1])
+        J1=(sigw**2 + inner_params[2]**2 * sigh**2)**-1*sum((h[i1]*inner_params[2]+inner_params[3]-w[i1])**2 )    
+        i2=h>=xbreak[1]
+        J2=(sigw**2 + inner_params[4]**2 * sigh**2)**-1*sum((h[i2]*inner_params[4]+inner_params[5]-w[i2])**2 )    
+        
+        J=J0+J1+J2    
+        
+        return J
+ 
+    def cons0_f(x):        
+        return x[0]*param_outer[0]+x[1]-x[2]*param_outer[0]-x[3] #this constraint requies this function to be equal to zero
+    def cons1_f(x):        
+        return x[2]*param_outer[1]+x[3]-x[4]*param_outer[1]-x[5] #this constraint requies this function to be equal to zero
+
+    
+    constraint0=optimize.NonlinearConstraint(cons0_f,0,0)
+    constraint1=optimize.NonlinearConstraint(cons1_f,0,0)
+        
+    constraints=[constraint0,constraint1]    
+
+    ShowDetailedOutput=Verbose
+    if not ReturnSolution:
+        ShowDetailedOutput=False
+
+    res = optimize.minimize(fun=SSE_inner,
+                    x0=init_params_inner,
+                    args=(param_outer,h,w,sigh,sigw),
+                    bounds=param_bounds_inner,
+                    method='trust-constr',
+                    constraints=constraints,
+                    options={'disp':ShowDetailedOutput,'maxiter':1e3,'verbose':0})    
+
+    if ReturnSolution:    
+        return res.fun,res.x
+    else:
+        return res.fun
+
+def plot3SDfit(h,w,params_inner,params_outer):
+    fig,ax = plt.subplots()
+    ax.scatter(h,w,marker='o')
+    plt.title('WSE vs width ')
+    plt.xlabel('WSE, m')
+    plt.ylabel('Width, m')
+
+    htest0=linspace(min(h),params_outer[0],10 )
+    wtest0=params_inner[0]*htest0+params_inner[1]
+    htest1=linspace(params_outer[0],params_outer[1],10)
+    wtest1= params_inner[2]*htest1+params_inner[3]
+    htest2=linspace(params_outer[1],max(h),10)
+    wtest2= params_inner[4]*htest2+params_inner[5]
+
+    plt.plot(htest0,wtest0,htest1,wtest1,htest2,wtest2)
+
+    plt.show()
+    return
